f19bd8344105a8e92cff9c1583af4a32eab0da2b
kevincbruce <kevincbruce2001@gmail.com>
2021-02-05 12:32:36 -0500

    Lab 2: About Page

---
+++ index.php
@@ -0,0 +1,41 @@
+<main style="background-color:rgb(53, 54, 56);">
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about" style="border: 3px solid grey;">
+        <h1 style="color:rgb(136, 0, 0); font-size: large;">About Us</h1>
+        <p>We are a review site for rock, metal, prog, and everything in between. We'll be sure to
+            give
+            you reviews of the latest and greatest gear and albums, along with all the classics! We are dedicated to
+            helping you keep up with your music news and rock on!
+        </p>
+    </div>
+
+    <div class="row">
+        <img src="images/KevinPortrait_20190403_0105-v2.jpg" style="text-align: center; size:400px; height:500px">
+        <div class="container"
+            style="width: 100%; height: fit-content; border: 3px solid grey;">
+            <h2 style="color:rgb(136, 0, 0); font-size: large;">CEO</h2>
+            <p>Kevin Bruce</p>
+            <p>kcb0018@mix.wvu.edu</p>
+            <p>Hello! I'm Kevin Bruce. I'm a Computer Science Sophomore at WVU, and started
+                designing this website for my software engineering course. I have also long had a passion for music,
+                especially rock and metal. As a bassist,
+                keyboardist, guitarist, songwriter, and amateur audio engineer, I have taken an interest to not only the
+                music, but the gear and technology
+                that allow for its creation. I hope to bring great information for other hobbyists on this site.</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="column">
+            <img src="images/R4L-Kevin.jpg" style="size:500px; height:600px">
+        </div>
+        <div class="column">
+            <img src="images/wallOfGuitars.jpeg" style="height:300px">
+        </div>
+        <div class="column">
+            <img src="images/randomSoundboard.jpeg" style="height:300px">
+        </div>
+    </div>
+
+</main>
