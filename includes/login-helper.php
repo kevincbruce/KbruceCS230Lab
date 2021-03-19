87ed98de539866e733ba05230c3878be94a9d165
kevincbruce <kevincbruce2001@gmail.com>
2021-03-19 19:09:12 -0400

    Pre-Lab 7 Material Submission

--- includes/login-helper.php
+++ includes/login-helper.php
@@ -1,19 +1,19 @@
 <?php
-if(isset($_POST["login-submit"])){
 
+if (isset($_POST['login-submit'])) {
     require 'dbhandler.php';
 
     $uname = $_POST['uname-email'];
-    $password = $_POST['pwd'];
+    $passw = $_POST['pwd'];
 
-    if(empty($uname) || empty($password)){
-        header("Location: .../login.php?error=EmptyField");
+    if (empty($uname) || empty($passw)) {
+        header("Location: ../login.php?error=EmptyField");
         exit();
     }
 
     $sql = "SELECT * FROM users WHERE uname=? OR email=?";
     $stmt = mysqli_stmt_init($conn);
-    if(!mysqli_stmt_prepare($stmt, $sql)){
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
         header("Location: ../login.php?error=SQLInjection");
         exit();
     }else{
@@ -22,31 +22,29 @@ if(isset($_POST["login-submit"])){
         $result = mysqli_stmt_get_result($stmt);
         $data = mysqli_fetch_assoc($result);
 
-        if(empty($data)){
+        if (empty($data)) {
             header("Location: ../login.php?error=UserDNE");
             exit();
         }
         else{
-            $pass_check = password_verify($password, $data['password']);
-            
+            $pass_check = password_verify($passw, $data['password']);
             if($pass_check == true){
-                session_start();
+                session_start(); 
                 $_SESSION['uid'] = $data['uid'];
                 $_SESSION['fname'] = $data['fname'];
                 $_SESSION['uname'] = $data['uname'];
 
+                //echo "<h1>Success!</h1><p>$uname</p>";
                 header("Location: ../profile.php?success=login");
                 exit();
-            }
-            else{
-                header("Location: ../login.php?error=WrongPassword");
+            }else{
+                header("Location: ../login.php?error=WrongPass");
                 exit();
             }
         }
     }
 
-}
-else{
+}else{
     header("Location: ../login.php");
     exit();
 }
